5c81950435351af30c035ef25e74ed3ffe555117
Yohannes Haile <yohannesakhaile@gmail.com>
2025-07-07 21:52:14 +0300

    Support Gemini summaries

---
+++ Sources/ClarioKit/ClarioKit/Providers/GeminiProvider.swift
@@ -0,0 +1,53 @@
+//
+//  GeminiProvider.swift
+//  ClarioKit
+//
+//  Created by Yohannes Haile on 7/7/25.
+//
+
+import Foundation
+
+public class GeminiProvider {
+    
+    var apiKey: String
+    
+    public init(apiKey: String) {
+        self.apiKey = apiKey
+    }
+    
+    @available(iOS 13.0, *)
+    func summarize(document: ClarioDocument) async throws -> String {
+        do {
+            let pdfData = try Data(contentsOf: document.sourceURL)
+            let base64PDF = pdfData.base64EncodedString()
+            
+            let promptPart = PartOrData.text(ClarioConstant.summarize)
+            let pdfPart = PartOrData.inlineData(.init(mimeType: "application/pdf", data: base64PDF))
+            
+            let requestBody = GenerateContentRequest(contents: [
+                GeminiContent(parts: [promptPart, pdfPart])
+            ])
+            
+            var urlComponents = URLComponents(string: ClarioConstant.geminiURL)!
+            urlComponents.queryItems = [
+                URLQueryItem(name: "key", value: self.apiKey)
+            ]
+            
+            guard let url = urlComponents.url else {
+                throw ClarioNetworkError.invalidURL
+            }
+            
+            let response: GenerateContentResponse = try await ClarioNetworkManager.shared.post(url: url, body: requestBody)
+            
+            if let summary = response.candidates.first?.content.parts.first?.text {
+               return summary
+            } else {
+                throw ClarioKitError.unavailableModel
+            }
+            
+        } catch(let error) {
+            throw error
+        }
+    }
+    
+}
---
+++ Sources/ClarioKit/ClarioKit/Utils/ClarioHTTPRequest.swift
@@ -0,0 +1,11 @@
+//
+//  ClarioHTTPRequest.swift
+//  ClarioKit
+//
+//  Created by Yohannes Haile on 7/7/25.
+//
+
+enum ClarioHTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
